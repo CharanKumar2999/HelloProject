d42da37eb9b0181b12f767360148aeb33b15481a
charankumar2999 <charankumar2999@gmail.com>
2023-06-30 20:11:27 +0530

    i am charan

---
+++ HelloProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
